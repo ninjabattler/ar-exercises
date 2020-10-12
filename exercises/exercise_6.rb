aa96bed63463b3c98f7598dbc45ffe6e496fe12a
ninjabattler <jaydenrtucker@gmail.com>
2020-10-12 21:11:36 +0000

    Exercise 6 adds five employees to each of the first two tables

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,14 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Khurram2", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Khurram3", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Khurram4", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Khurram5", last_name: "Virani", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Carrie", last_name: "Larry", hourly_rate: 60)
+@store2.employees.create(first_name: "Carrie2", last_name: "Larry", hourly_rate: 60)
+@store2.employees.create(first_name: "Carrie3", last_name: "Larry", hourly_rate: 60)
+@store2.employees.create(first_name: "Carrie4", last_name: "Larry", hourly_rate: 60)
+@store2.employees.create(first_name: "Carrie5", last_name: "Larry", hourly_rate: 40)
