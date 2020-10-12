f8aa34af76640582dec1339cddc8da4117324838
ninjabattler <jaydenrtucker@gmail.com>
2020-10-12 20:52:11 +0000

    Exercise 4 finds all stores with mens clothes and all stores with women clothes + annual revenue less than 1000000 and print them to the console

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,27 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+surrey = Store.new(name: "Surrey",  annual_revenue: 224000, mens_apparel: false, womens_apparel: true) 
+surrey.save
+
+whistler = Store.new(name: "Whistler",  annual_revenue: 1900000, mens_apparel: true, womens_apparel: false) 
+whistler.save
+
+yaletown = Store.new(name: "Yaletown",  annual_revenue: 430000, mens_apparel: true, womens_apparel: true) 
+yaletown.save
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do | st |
+  puts st.name
+  puts st.annual_revenue
+  puts "<--->"
+end
+
+@womens_stores = Store.where('annual_revenue < ?', 1000000).where( womens_apparel: true)
+
+@womens_stores.each do | st2 |
+  puts st2.name
+  puts st2.annual_revenue
+  puts "<--->"
+end
